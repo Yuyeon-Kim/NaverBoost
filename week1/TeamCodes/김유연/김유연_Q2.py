6e492a2089cf1a80d6394fa3ab4a4cfa4d20670e
Yuyeon-Kim <1rladbdus@gmail.com>
2022-01-18 15:51:16 +0900

    김유연님 코드 추가

---
+++ week1/TeamCodes/김유연/김유연_Q2.py
@@ -0,0 +1,14 @@
+# 220113
+# 문제: 
+# string 문장을 받아 단어를 역순으로 출력하는 함수를 작성하세요.
+
+sentence = "way a is there will a is there where"
+
+def reverse_sentence(sentence):
+    # TODO
+    return " ".join(reversed(sentence.split()))
+
+print(reverse_sentence(sentence))
+
+# 출력
+# where there is a will there is a way
